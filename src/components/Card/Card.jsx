c8396bc1399b75600014e6cb390896729c30427f
1Santiiago <santiagoferreirati@gmail.com>
2023-07-18 19:56:52 -0300

    editCard

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,31 @@
-import * as c from "./styleCard";
+import React, { useState } from 'react';
+import * as c from './styleCard'
+function Card(props) {
+  const [editMode, setEditMode] = useState(false);
+  const [name, setName] = useState(props.name);
+  const [tel, setTel] = useState(props.tel);
+  const [email, setEmail] = useState(props.email);
 
-export default function Card({ name, tel, email }) {
+  const handleSave = () => {
+    
+    setEditMode(false);
+  };
+
   return (
     <c.Container>
-      <c.Title>Contato</c.Title>
-      <c.Text>Nome: {name}</c.Text>
-      <c.Text>Tel: {tel}</c.Text>
-      <c.Text>Email: {email}</c.Text>
-      <button>Editar</button>
+      <c.Text>
+        Nome: {editMode ? <input value={name} onChange={(e) => setName(e.target.value)} /> : name}
+      </c.Text>
+      <c.Text>
+        Tel: {editMode ? <input value={tel} onChange={(e) => setTel(e.target.value)} /> : tel}
+      </c.Text>
+      <c.Text>
+        Email: {editMode ? <input value={email} onChange={(e) => setEmail(e.target.value)} /> : email}
+      </c.Text>
+      <button onClick={() => setEditMode(!editMode)}>{editMode ? "Cancelar" : "Editar"}</button>
+      {editMode && <button onClick={handleSave}>Salvar</button>}
     </c.Container>
   );
 }
+
+export default Card;
