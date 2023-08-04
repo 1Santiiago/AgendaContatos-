35e0215618cfea0c0ffd5b9755353940ddffb568
1Santiiago <santiagoferreirati@gmail.com>
2023-08-04 00:50:32 -0300

    Tudo certo

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
+// Componente Form.js
+import React, { useState } from "react";
 import * as c from "./styleForm";
 import Card from "../Card/Card";
+
 export default function Form() {
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
   const [email, setEmail] = useState("");
   const [submitted, setSubmitted] = useState(false);
-  const [dataList, setDataList] = useState([])
+  const [dataList, setDataList] = useState([]);
+
   const handleSignup = (e) => {
     e.preventDefault();
-    setSubmitted({ [submitted]: true });
+    setSubmitted(true);
 
     const newData = { name, number, email };
     setDataList([...dataList, newData]);
@@ -18,11 +21,16 @@ export default function Form() {
     setEmail("");
   };
 
+  const handleDeleteCard = (index) => {
+    const updatedDataList = dataList.filter((_, i) => i !== index);
+    setDataList(updatedDataList);
+  };
+
   return (
     <>
       <c.Container>
         <c.Form onSubmit={handleSignup}>
-          <c.Title>Formulario de Contato</c.Title>
+          <c.Title>Formulário de Contato</c.Title>
 
           <c.Input
             type="text"
@@ -34,7 +42,7 @@ export default function Form() {
 
           <c.Input
             type="tel"
-            placeholder="Numero Telefone"
+            placeholder="Número Telefone"
             required
             value={number}
             onChange={(e) => setNumber(e.target.value)}
@@ -50,22 +58,23 @@ export default function Form() {
 
           <c.Button>Enviar</c.Button>
         </c.Form>
-
       </c.Container>
+
       <c.Container>
         <c.Wraper>
-
-
-
-
           {submitted && (
             <c.ContainerCard>
               {dataList.map((data, index) => (
-                <Card name={data.name} tel={data.number} email={data.email} key={index} />
+                <Card
+                  name={data.name}
+                  tel={data.number}
+                  email={data.email}
+                  key={index}
+                  onDelete={() => handleDeleteCard(index)} 
+                />
               ))}
             </c.ContainerCard>
           )}
-
         </c.Wraper>
       </c.Container>
     </>
