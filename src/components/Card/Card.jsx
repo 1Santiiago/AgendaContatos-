454d7e9061f7e6702cd393e9c4c401653facc0d6
1Santiiago <santiagoferreirati@gmail.com>
2023-08-04 00:49:34 -0300

    Botao delete feito

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,37 +1,52 @@
-import React, { useState } from 'react';
-import { FaTrash } from 'react-icons/fa';
+// Componente Card.js
+import React, { useState } from "react";
+import { FaTrash } from "react-icons/fa";
 
-import * as c from './styleCard'
+import * as c from "./styleCard";
+
 function Card(props) {
   const [editMode, setEditMode] = useState(false);
   const [name, setName] = useState(props.name);
   const [tel, setTel] = useState(props.tel);
   const [email, setEmail] = useState(props.email);
-  const [deleteMode, setDeleteMode] = useState(false);
-
-
-
 
   const handleSave = () => {
-
     setEditMode(false);
   };
 
   return (
     <c.Container>
       <c.Text>
-        Nome: {editMode ? <input value={name} onChange={(e) => setName(e.target.value)} /> : name}
+        Nome:{" "}
+        {editMode ? (
+          <input value={name} onChange={(e) => setName(e.target.value)} />
+        ) : (
+          name
+        )}
       </c.Text>
       <c.Text>
-        Tel: {editMode ? <input value={tel} onChange={(e) => setTel(e.target.value)} /> : tel}
+        Tel:{" "}
+        {editMode ? (
+          <input value={tel} onChange={(e) => setTel(e.target.value)} />
+        ) : (
+          tel
+        )}
       </c.Text>
       <c.Text>
-        Email: {editMode ? <input value={email} onChange={(e) => setEmail(e.target.value)} /> : email}
+        Email:{" "}
+        {editMode ? (
+          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        ) : (
+          email
+        )}
       </c.Text>
-      <button onClick={() => setEditMode(!editMode)}>{editMode ? "Cancelar" : "Editar"}</button>
+      <button onClick={() => setEditMode(!editMode)}>
+        {editMode ? "Cancelar" : "Editar"}
+      </button>
       {editMode && <button onClick={handleSave}>Salvar</button>}
-      <button onClick={() => setDeleteMode(!deleteMode)}>
-        {deleteMode ? <FaTrash /> : <FaTrash />}
+
+      <button onClick={props.onDelete}>
+        <FaTrash />
       </button>
     </c.Container>
   );
